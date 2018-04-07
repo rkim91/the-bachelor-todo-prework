747c9d836919710e8f847e7158d6a2a8190b8e86
Richard Kim <richeekim@gmail.com>
2018-04-07 00:45:59 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data.each do |season, person|
     person.each do |details, values|
-      if values == "Winner"
-        return :name
-      end
+      return values
     end
   end
 end
